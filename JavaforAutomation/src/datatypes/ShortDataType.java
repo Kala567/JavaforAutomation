918b9004170aa982fc180b043a4cf36d4dd32db6
ghrao <ghrao@LAPTOP-4CIGH20I.lan>
2020-12-01 16:31:09 -0500

    Deleted sysout Exit statement

--- JavaforAutomation/src/datatypes/ShortDataType.java
+++ JavaforAutomation/src/datatypes/ShortDataType.java
@@ -10,7 +10,6 @@ public class ShortDataType {
 		short year = 2020;
 		
 		System.out.println( "Short Data Type Value is: " + year);
-		System.out.println( "Short Data Type Value is exit: " );
 
 	}
 
